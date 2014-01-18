700bb562b3bad1419ff9d9fc578506e113b7b0f9
Jason Lefler <leflerja@arctic.cse.msu.edu>
2014-01-18 15:37:15 -0500

    Through 2a of homework 2

--- server.py
+++ server.py
@@ -3,34 +3,37 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn()     # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+def handle_connection(conn, r_line, c_type, r_body):
+  conn.send(r_line)
+  conn.send(c_type)
+  conn.send(r_body)
+  conn.close()
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+def main():
+  s = socket.socket()         # Create a socket object
+  host = socket.getfqdn()     # Get local machine name
+  port = random.randint(8000, 9999)
+  s.bind((host, port))        # Bind to the port
 
-s.listen(5)                 # Now wait for client connection.
+  print 'Starting server on', host, port
+  print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
+  s.listen(5)                 # Now wait for client connection
+
+  print 'Entering infinite loop; hit CTRL-C to exit'
+  while True:
+    # Establish connection with client
     c, (client_host, client_port) = s.accept()
     print c.recv(1000)
     print 'Got connection from', client_host, client_port
 
-    # New code for hw2
-    response_type = "HTTP/1.0"
-    response_line = "200 OK\r\n"
-    content_type = "Content-Type: text/html\r\n\r\n"
-    response_body = "<html><body> \
-                     <h1>Hello, world</h1> this is leflerja's Web server \
-                     </body></html>"
+    r_line = "HTTP/1.0 200 OK\r\n"
+    c_type = "Content-Type: text/html\r\n\r\n"
+    r_body = "<html><body> \
+              <h1>Hello, world</h1> this is leflerja's Web server \
+              </body></html>"
 
-    c.send(response_type + ' ' + response_line)
-    c.send(content_type)
-    c.send(response_body)
+    handle_connection(c, r_line, c_type, r_body)
 
-    c.close()
-
+if __name__ == '__main__':
+  main()
