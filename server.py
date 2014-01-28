c618d41673a5d58f5e47f9c361d5b57442ae34f0
Jason Lefler <leflerja@arctic.cse.msu.edu>
2014-01-28 15:08:21 -0500

    Finished POST stuff for homework 3

--- server.py
+++ server.py
@@ -4,7 +4,15 @@ import time
 from urlparse import urlparse, parse_qs
 import urllib
 
-def request_index(conn, path):
+# Global variables
+okay_header = 'HTTP/1.0 200 OK\r\n'
+footer = '</body></html>\r\n'
+text_content = 'Content-type: text/html\r\n\r\n' + \
+             '<html><body>\r\n'
+post_content = 'Content-type: application/x-www-form-urlencoded\r\n\r\n' + \
+               '<html><body>\r\n'
+
+def get_index(conn, params):
     return '<h1>Hello World!</h1>\r\n' + \
            "This is leflerja's Web server<br>\r\n" + \
            "<a href='/content'>Content</a><br>\r\n" + \
@@ -12,19 +20,19 @@ def request_index(conn, path):
            "<a href='/images'>Images</a><br>\r\n" + \
            "<a href='/form'>Form</a>\r\n"
 
-def request_content(conn, path):
+def get_content(conn, params):
     return '<h1>Content Page</h1>\r\n' + \
            'This is the content page\r\n'
 
-def request_files(conn, path):
+def get_files(conn, params):
     return '<h1>Files Page</h1>\r\n' + \
            'This is the files page\r\n'
 
-def request_images(conn, path):
+def get_images(conn, params):
     return '<h1>Images Page</h1>\r\n' + \
            'This is the images page\r\n'
 
-def request_form(conn, path):
+def get_form(conn, params):
     return '<h1>Form Page</h1>\r\n' + \
            '<form action=\'/submit\' method=\'GET\'>\r\n' + \
            'First Name: <input type=\'text\' name=\'firstname\'><br>\r\n' + \
@@ -32,44 +40,78 @@ def request_form(conn, path):
            '<input type=\'submit\' name=\'submit\'>\r\n' + \
            '</form>\r\n'
 
-def request_submit(conn, path):
+def get_submit(conn, params):
     return '<h1>Submit Page</h1>\r\n' + \
-           'Hello {0} {1}'.format(path['firstname'][0], path['lastname'][0])
+           'Hello {0} {1}'.format(params['firstname'][0], params['lastname'][0])
 
-def get_request(conn, path):
-    form_keys = parse_qs(urlparse(path)[4])
+def handle_get(conn, path):
+    params = parse_qs(urlparse(path)[4])
     page = urlparse(path)[2]
 
-    header = 'HTTP/1.0 200 OK\r\n' + \
-             'Content-type: text/html\r\n\r\n' + \
-             '<html><body>\r\n'
-    footer = '</body></html>\r\n'
+    # Not sure why I keep getting /favicon.ico... I'll deal with it later
+    get_pages = {'/'            : get_index,   \
+                 '/favicon.ico' : get_index,   \
+                 '/content'     : get_content, \
+                 '/files'       : get_files,   \
+                 '/images'      : get_images,  \
+                 '/form'        : get_form,    \
+                 '/submit'      : get_submit   }
 
-    # Not sure why I keep getting /favicon.ico...
-    options = {'/'            : request_index,   \
-               '/favicon.ico' : request_index,   \
-               '/content'     : request_content, \
-               '/files'       : request_files,   \
-               '/images'      : request_images,  \
-               '/form'        : request_form,    \
-               '/submit'      : request_submit   }
-
-    conn.send(header)
-    conn.send(options[page](conn, form_keys))
+    conn.send(okay_header)
+    conn.send(text_content)
+    conn.send(get_pages[page](conn, params))
     conn.send(footer)
 
-def post_request(conn):
-    conn.send('HTTP/1.0 200 OK\r\n\r\n')
-    conn.send('This is a post request')
+def post_request(conn, request):
+    return 'HTTP/1.0 200 OK\r\n\r\n' + \
+           'This is a post request'
+
+def post_form(conn, params):
+    return '<h1>Form Page</h1>\r\n' + \
+           '<form action=\'/submit\' method=\'POST\'>\r\n' + \
+           'First Name: <input type=\'text\' name=\'firstname\'><br>\r\n' + \
+           'Last Name: <input type=\'text\' name=\'lastname\'><br>\r\n' + \
+           '<input type=\'submit\' name=\'submit\'>\r\n' + \
+           '</form>\r\n'
+
+def post_submit(conn, params):
+    return '<h1>Submit Page</h1>\r\n' + \
+           'Hello {0} {1}'.format(params['firstname'][0], params['lastname'][0])
+
+def handle_post(conn, request):
+    headers = []
+    body = ""
+    has_body = False
+
+    for line in request.split("\r\n"):
+        if has_body:
+            body = line
+            continue
+        if line == "":
+            has_body = True
+        headers.append(line)
+
+    path = request.split()[1]
+    page = urlparse(path)[2]
+    params = parse_qs(body)
+
+    post_pages = {'/'       : post_request, \
+                  '/form'   : post_form,    \
+                  '/submit' : post_submit   }
+
+    conn.send(okay_header)
+    conn.send(post_content)
+    conn.send(post_pages[page](conn, params))
+    conn.send(footer)
 
 def handle_connection(conn):
     request = conn.recv(1000)
     request_type = request.split()[0]
     if request_type == 'GET':
         path = request.split()[1]
-        get_request(conn, path)
+        handle_get(conn, path)
     elif request_type == 'POST':
-        post_request(conn)
+         handle_post(conn, request)
     conn.close()
 
 def main():
--- test_server.py
+++ test_server.py
@@ -1,9 +1,12 @@
 import server
 
 # These are the global test variables
-header = 'HTTP/1.0 200 OK\r\n' + \
-         'Content-type: text/html\r\n\r\n' + \
-         '<html><body>\r\n'
+get_header = 'HTTP/1.0 200 OK\r\n' + \
+             'Content-type: text/html\r\n\r\n' + \
+             '<html><body>\r\n'
+post_header = 'HTTP/1.0 200 OK\r\n' + \
+              'Content-type: application/x-www-form-urlencoded\r\n\r\n' + \
+              '<html><body>\r\n'
 footer = '</body></html>\r\n'
 
 class FakeConnection(object):
@@ -40,7 +43,7 @@ def test_get_root():
                   "<a href='/files'>Files</a><br>\r\n" + \
                   "<a href='/images'>Images</a><br>\r\n" + \
                   "<a href='/form'>Form</a>\r\n"
-    expected_return = header + root_return + footer
+    expected_return = get_header + root_return + footer
 
     server.handle_connection(conn)
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
@@ -50,7 +53,7 @@ def test_get_content():
     conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
     content_return = '<h1>Content Page</h1>\r\n' + \
                      'This is the content page\r\n'
-    expected_return = header + content_return + footer
+    expected_return = get_header + content_return + footer
 
     server.handle_connection(conn)
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
@@ -60,7 +63,7 @@ def test_get_file():
     conn = FakeConnection("GET /files HTTP/1.0\r\n\r\n")
     file_return = '<h1>Files Page</h1>\r\n' + \
                   'This is the files page\r\n'
-    expected_return = header + file_return + footer
+    expected_return = get_header + file_return + footer
 
     server.handle_connection(conn)
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
@@ -70,7 +73,7 @@ def test_get_images():
     conn = FakeConnection("GET /images HTTP/1.0\r\n\r\n")
     images_return = '<h1>Images Page</h1>\r\n' + \
                     'This is the images page\r\n'
-    expected_return = header + images_return + footer
+    expected_return = get_header + images_return + footer
 
     server.handle_connection(conn)
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
@@ -84,7 +87,7 @@ def test_get_form():
                   'Last Name: <input type=\'text\' name=\'lastname\'><br>\r\n' + \
                   '<input type=\'submit\' name=\'submit\'>\r\n' + \
                   '</form>\r\n'
-    expected_return = header + form_return + footer
+    expected_return = get_header + form_return + footer
 
     server.handle_connection(conn)
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
@@ -94,14 +97,42 @@ def test_get_submit():
     conn = FakeConnection("GET /submit?firstname=Jason&lastname=Lefler&submit=Submit HTTP/1.0\r\n\r\n")
     submit_return = '<h1>Submit Page</h1>\r\n' + \
                     'Hello Jason Lefler'
-    expected_return = header + submit_return + footer
+    expected_return = get_header + submit_return + footer
 
     server.handle_connection(conn)
     assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
      
-
 # Test a POST call
-def test_post():
+def test_post_request():
     conn = FakeConnection("POST / HTTP/1.1\r\n\r\n")
-    expected_return = 'HTTP/1.0 200 OK\r\n\r\n' + \
-                      'This is a post request'
+    post_return = 'HTTP/1.0 200 OK\r\n\r\n' + \
+                  'This is a post request'
+    expected_return = post_header + post_return + footer
+
+    server.handle_connection(conn)
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+# Test a POST call for the form page
+def test_post_form():
+    conn = FakeConnection("POST /form HTTP/1.0\r\n\r\n")
+    form_return = '<h1>Form Page</h1>\r\n' + \
+                  '<form action=\'/submit\' method=\'POST\'>\r\n' + \
+                  'First Name: <input type=\'text\' name=\'firstname\'><br>\r\n' + \
+                  'Last Name: <input type=\'text\' name=\'lastname\'><br>\r\n' + \
+                  '<input type=\'submit\' name=\'submit\'>\r\n' + \
+                  '</form>\r\n'
+    expected_return = post_header + form_return + footer
+
+    server.handle_connection(conn)
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+# Test a POST call for the submit page
+def test_get_submit():
+    conn = FakeConnection("POST /submit HTTP/1.0\r\nHost: mse.edu\r\n\r\nfirstname=Jason&lastname=Lefler")
+    submit_return = '<h1>Submit Page</h1>\r\n' + \
+                    'Hello Jason Lefler'
+    expected_return = post_header + submit_return + footer
+
+    server.handle_connection(conn)
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+     
