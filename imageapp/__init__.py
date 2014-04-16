6108d7885f80aff58f78c42d5ee851201c17e6f1
Matheus Araujo <araujoma@msu.edu>
2014-04-16 03:03:16 -0400

    Imageapp As SQL

--- imageapp/__init__.py
+++ imageapp/__init__.py
@@ -5,6 +5,9 @@ from quixote.publish import Publisher
 # this imports the class RootDirectory from the file 'root.py'
 from .root import RootDirectory
 from . import html, image, javascript, css
+import os
+
+
 
 def create_publisher():
      p = Publisher(RootDirectory(), display_exceptions='plain')
@@ -12,11 +15,15 @@ def create_publisher():
      return p
  
 def setup():                            # stuff that should be run once.
+    os.system("bash create_db.sh")
     html.init_templates()
     javascript.init_javascript()
     css.init_css()
 
     some_data = open('imageapp/dice.png', 'rb').read()
+    image.add_image(some_data)
+
+    some_data = open('imageapp/tux.png', 'rb').read()
     image.add_image(some_data)
     
 
--- imageapp/image.py
+++ imageapp/image.py
@@ -2,48 +2,122 @@
 from PIL import Image
 from StringIO import StringIO
 import base64
+import sqlite3 as lite
 
-images = {}
-thumbs = {}
+# images = {}
+# thumbs = {}
+DB_path = "imageapp.db"
 
-
-def add_image(data):
-    img = Image.open(StringIO(data))
-
-    if images:
-        image_num = max(images.keys()) + 1
-
-    else:
-        image_num = 0
-        
-    images[image_num] = data
-    
+def create_thumbnail(data):
     #Resize same ratio
+    img = Image.open(StringIO(data))
     basewidth = 200
     wpercent = (basewidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-    
+
+    #Trick way to get img, I did not figureout who te get image code from img variable without doing that
     img.save("tmp.png")
     f_img = open("tmp.png", "r")
     
-    thumbs[image_num] = base64.encodestring(f_img.read())
+    #Encode thumbs in base64
+    thumb = base64.encodestring(f_img.read())
+    return thumb
 
-    return image_num
+def add_image(data):
+
+    thumb = create_thumbnail(data)
+   
+    try:
+        con = lite.connect(DB_path)
+        cur = con.cursor()
+        #Get user_id
+        user_id = cur.execute("select max(user_id) from images").fetchone()[0]
+        if user_id == None:
+            user_id = 0
+        else:
+            user_id += 1
+
+        cur.execute('INSERT INTO images (image,thumb,user_id) VALUES (?, ?, ?)',[lite.Binary(data), thumb, user_id])
+        con.commit()
+        
+    except lite.Error, e:
+        print "Error:" + str(e)
+    finally:
+        if con:
+            con.close()
+
+    return user_id
 
 def get_image(num):
-    return images[num]
+    try:
+        con = lite.connect(DB_path)
+        cur = con.cursor()
+        image = cur.execute('SELECT image from images where user_id=%d' % num).fetchone()[0]
+    
+    except lite.Error, e:
+        print "Error:" + str(e)
+
+    finally:
+        if con:
+            con.close()
+
+    return image
 
 def get_latest_image():
-    image_num = max(images.keys())
-    return images[image_num]
+    try:
+        con = lite.connect(DB_path)
+        cur = con.cursor()
+        image = cur.execute('SELECT image FROM images ORDER BY id DESC LIMIT 1;').fetchone()[0]
+
+    except lite.Error, e:
+        print "Error:" + str(e)
+
+    finally:
+        if con:
+            con.close()
+    
+    return image
 
 def delete_image(num):
     images.pop(num)
     return "Done"
 
 def has_image(num):
-    return images.has_key(num)
+    try:
+        con = lite.connect(DB_path)
+        cur = con.cursor()
+        exists = cur.execute('select 1 from images where user_id = ?', [num]).fetchone()
+
+    except lite.Error, e:
+        print "Error:" + str(e)
+
+    finally:
+        if con:
+            pass
+            # con.close()
+    
+    if exists == None:
+        return False
+    else:
+        return True
 
 def get_thumbs_list():
+    try:
+        con = lite.connect(DB_path)
+        cur = con.cursor()
+        thumbs_query = cur.execute("select thumb from images").fetchall()
+        pass
+
+    except lite.Error, e:
+        print "Error:" + str(e)
+
+    finally:
+        if con:
+            con.close()
+
+    thumbs = []
+    for item in thumbs_query:
+        thumbs.append(item[0])
+        
     return thumbs
